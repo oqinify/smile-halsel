682f1d34aed4012a2f28e5ec46eece41c80ca56b
Oqinify <khoirul.stark@gmail.com>
2021-06-21 18:17:11 +0700

    Add files via upload

---
+++ sendTopicPush.php
@@ -0,0 +1,47 @@
+<?php 
+//importing required files 
+require_once 'Firebase.php';
+require_once 'Push.php'; 
+  
+$response = array(); 
+ 
+if($_SERVER['REQUEST_METHOD']=='POST'){ 
+ //hecking the required params 
+ if(isset($_POST['title']) and isset($_POST['message'])){
+ 
+ //creating a new push
+ $push = null; 
+ //first check if the push has an image with it
+ if(isset($_POST['image'])){
+ $push = new Push(
+ $_POST['title'],
+ $_POST['message'],
+ $_POST['image']
+ );
+ }else{
+ //if the push don't have an image give null in place of image
+ $push = new Push(
+ $_POST['title'],
+ $_POST['message'],
+ null
+ );
+ }
+ 
+ //getting the push from push object
+ $mPushNotification = $push->getPush(); 
+
+ //creating firebase class object 
+ $firebase = new Firebase(); 
+ 
+ //sending push notification and displaying result 
+ echo $firebase->sendToTopic('global', $mPushNotification);
+ }else{
+ $response['error']=true;
+ $response['message']='Parameters missing';
+ }
+}else{
+ $response['error']=true;
+ $response['message']='Invalid request';
+}
+ 
+echo json_encode($response);
