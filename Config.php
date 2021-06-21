3834ed97a61b5bc1d4117573c68a49cc5c6c8ac4
Oqinify <khoirul.stark@gmail.com>
2021-06-21 10:08:06 +0700

    Add files via upload

---
+++ Config.php
@@ -0,0 +1,8 @@
+<?php 
+ define('DB_USERNAME','root');
+ define('DB_PASSWORD','');
+ define('DB_NAME','account_device');
+ define('DB_HOST','localhost');
+ 
+ // Firebase API Key
+define('FIREBASE_API_KEY', 'AAAAV7bAeR8:APA91bHXB8BrNtegI3D-ibevDfVOZjVG7Zuzc9f1rJF6OtJilYEs1AEwaKD57YRUSPWbBMvQ_4xoE8Ck_D35zYjb7_tZyafE0uamU8CN6rIPshmJUwGrzY8uKGxA7c1d5tNRh_wu-QNN');
